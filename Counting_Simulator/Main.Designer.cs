748a2fd88f5ad3f628b37a3f235c4b4ccaf8f945
GJOOF <134306936+GJOOF@users.noreply.github.com>
2023-06-14 11:11:43 +0300

    Add files via upload

---
+++ Counting_Simulator/Main.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Counting_Simulator
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Main));
+            this.lab_timer = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tb_answer = new System.Windows.Forms.TextBox();
+            this.btn_add = new System.Windows.Forms.Button();
+            this.lab_equation_done = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lab_equation = new System.Windows.Forms.Label();
+            this.btn_sub = new System.Windows.Forms.Button();
+            this.btn_mult = new System.Windows.Forms.Button();
+            this.btn_div = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lab_timer
+            // 
+            this.lab_timer.AutoSize = true;
+            this.lab_timer.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lab_timer.Location = new System.Drawing.Point(230, 9);
+            this.lab_timer.Name = "lab_timer";
+            this.lab_timer.Size = new System.Drawing.Size(50, 20);
+            this.lab_timer.TabIndex = 0;
+            this.lab_timer.Text = "Timer";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.progressBar1.Location = new System.Drawing.Point(12, 32);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(257, 27);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(210, 65);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(70, 44);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Enter";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tb_answer
+            // 
+            this.tb_answer.Location = new System.Drawing.Point(98, 74);
+            this.tb_answer.Name = "tb_answer";
+            this.tb_answer.Size = new System.Drawing.Size(106, 22);
+            this.tb_answer.TabIndex = 3;
+            // 
+            // btn_add
+            // 
+            this.btn_add.Location = new System.Drawing.Point(12, 157);
+            this.btn_add.Name = "btn_add";
+            this.btn_add.Size = new System.Drawing.Size(128, 43);
+            this.btn_add.TabIndex = 4;
+            this.btn_add.Text = "Addition";
+            this.btn_add.UseVisualStyleBackColor = true;
+            this.btn_add.Click += new System.EventHandler(this.btn_add_Click);
+            // 
+            // lab_equation_done
+            // 
+            this.lab_equation_done.AutoSize = true;
+            this.lab_equation_done.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lab_equation_done.Location = new System.Drawing.Point(132, 9);
+            this.lab_equation_done.Name = "lab_equation_done";
+            this.lab_equation_done.Size = new System.Drawing.Size(39, 20);
+            this.lab_equation_done.TabIndex = 5;
+            this.lab_equation_done.Text = "N/A";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(114, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Equation done:";
+            // 
+            // lab_equation
+            // 
+            this.lab_equation.AutoSize = true;
+            this.lab_equation.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lab_equation.Location = new System.Drawing.Point(8, 76);
+            this.lab_equation.Name = "lab_equation";
+            this.lab_equation.Size = new System.Drawing.Size(71, 20);
+            this.lab_equation.TabIndex = 7;
+            this.lab_equation.Text = "Equation";
+            // 
+            // btn_sub
+            // 
+            this.btn_sub.Location = new System.Drawing.Point(141, 157);
+            this.btn_sub.Name = "btn_sub";
+            this.btn_sub.Size = new System.Drawing.Size(128, 43);
+            this.btn_sub.TabIndex = 8;
+            this.btn_sub.Text = "Subtraction";
+            this.btn_sub.UseVisualStyleBackColor = true;
+            this.btn_sub.Click += new System.EventHandler(this.btn_sub_Click);
+            // 
+            // btn_mult
+            // 
+            this.btn_mult.Location = new System.Drawing.Point(12, 206);
+            this.btn_mult.Name = "btn_mult";
+            this.btn_mult.Size = new System.Drawing.Size(128, 43);
+            this.btn_mult.TabIndex = 9;
+            this.btn_mult.Text = "Multiplication";
+            this.btn_mult.UseVisualStyleBackColor = true;
+            this.btn_mult.Click += new System.EventHandler(this.btn_mult_Click);
+            // 
+            // btn_div
+            // 
+            this.btn_div.Location = new System.Drawing.Point(141, 206);
+            this.btn_div.Name = "btn_div";
+            this.btn_div.Size = new System.Drawing.Size(128, 43);
+            this.btn_div.TabIndex = 10;
+            this.btn_div.Text = "Division";
+            this.btn_div.UseVisualStyleBackColor = true;
+            this.btn_div.Click += new System.EventHandler(this.btn_div_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(209, 9);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(15, 20);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 301);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.btn_div);
+            this.Controls.Add(this.btn_mult);
+            this.Controls.Add(this.btn_sub);
+            this.Controls.Add(this.lab_equation);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lab_equation_done);
+            this.Controls.Add(this.btn_add);
+            this.Controls.Add(this.tb_answer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.lab_timer);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Main";
+            this.Text = "Counting SImulator";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lab_timer;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox tb_answer;
+        private System.Windows.Forms.Button btn_add;
+        private System.Windows.Forms.Label lab_equation_done;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lab_equation;
+        private System.Windows.Forms.Button btn_sub;
+        private System.Windows.Forms.Button btn_mult;
+        private System.Windows.Forms.Button btn_div;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Counting_Simulator/Main.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Counting_Simulator
+{
+    public partial class Main : Form
+    {
+
+        int Max_Try = 20;
+        int Try;
+        Random random = new Random();
+        string Operator;
+        int answer;
+        int RightAnswer;
+        int WrongAnswer;
+        int timeForTimer;
+        int minuteTimeForTimer;
+        string AllAnswers;
+        public Main()
+        {
+            InitializeComponent();
+            tb_answer.Enabled = false;
+        }
+        private void Start()
+        {
+            btn_add.Visible = false;
+            btn_sub.Visible = false;
+            btn_mult.Visible = false;
+            btn_div.Visible = false;
+            tb_answer.Text = "";
+            tb_answer.Focus();
+            Try = 0;
+            Consider();
+        }
+        private void Consider()
+        {
+            timer1.Enabled = true;
+            tb_answer.Text = "";
+            tb_answer.Focus();
+            if (Try <= Max_Try)
+            {
+                int a = random.Next(1, 20);
+                int b = random.Next(1, 20);
+                int a_2 = random.Next(1, 10);
+                int b_2 = random.Next(1, 10);
+
+                if (Operator == "Addition")
+                {
+                    string a_text = Convert.ToString(a, 2);
+                    string b_text = Convert.ToString(b, 2);
+                    answer = Convert.ToInt32(Convert.ToString(Convert.ToByte(a_text, 2) + Convert.ToByte(b_text, 2), 2), 2);
+                    lab_equation.Text = $"{a.ToString()} + {b.ToString()}  =";
+                }
+                if (Operator == "Subtraction")
+                {
+                    if (a < b)
+                    {
+                        int x = a;
+                        a = b;
+                        b = x;
+                    }
+                    string a_text = Convert.ToString(a, 2);
+                    string b_text = Convert.ToString(b, 2);
+                    answer = Convert.ToInt32(Convert.ToString(Convert.ToByte(a_text, 2) - Convert.ToByte(b_text, 2), 2), 2);
+                    lab_equation.Text = $"{a.ToString()} - {b.ToString()}  =";
+                }
+                if (Operator == "Multiplication")
+                {
+                    string a_text = Convert.ToString(a_2, 2);
+                    string b_text = Convert.ToString(b_2, 2);
+                    answer = Convert.ToInt32(Convert.ToString(Convert.ToByte(a_text, 2) * Convert.ToByte(b_text, 2), 2), 2);
+                    lab_equation.Text = $"{a_2.ToString()} * {b_2.ToString()}  =";
+                }
+                if (Operator == "Division")
+                {
+                    if (a_2 < b_2)
+                    {
+                        int x_2 = a_2;
+                        a_2 = b_2;
+                        b_2 = x_2;
+                    }
+                    string a_text = Convert.ToString(a_2, 2);
+                    string b_text = Convert.ToString(b_2, 2);
+                    answer = Convert.ToInt32(Convert.ToString(Convert.ToByte(a_text, 2)));
+                    lab_equation.Text = $"{Convert.ToString(Convert.ToByte(a_text, 2) * (Convert.ToByte(b_text, 2)))} : {Convert.ToString(Convert.ToByte(b_text, 2))} =";
+                }
+            }
+            else
+            {
+                timer1.Stop();
+                MessageBox.Show($" Gived answers : {RightAnswer + WrongAnswer} \n" +
+               $" Correct answers: {RightAnswer}\n" +
+               $" Wrong answers: {WrongAnswer}", "Statistics",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.Information);
+                Done();
+            }
+
+            lab_equation_done.Text = (RightAnswer + WrongAnswer).ToString();
+        }
+        private void Done()
+        {
+            tb_answer.Focus();
+            Try = 0;
+            AllAnswers = (RightAnswer + WrongAnswer).ToString();
+            lab_equation_done.Text = AllAnswers;
+            lab_equation.Text = "Equation";
+            MessageBox.Show($" Gived answers : {AllAnswers} \n" +
+                $" Correct answers: {RightAnswer}\n" +
+                $" Wrong answers: {WrongAnswer}", "Statistics",
+                MessageBoxButtons.OK,
+                MessageBoxIcon.Information);
+            btn_add.Visible = true;
+            btn_sub.Visible = true;
+            btn_mult.Visible = true;
+            btn_div.Visible = true;
+            WrongAnswer = 0;
+            RightAnswer = 0;
+        }
+
+        private void btn_add_Click(object sender, EventArgs e)
+        {
+            Operator = "Addition";
+            tb_answer.Focus();
+            tb_answer.Enabled = true;
+            Start();
+        }
+
+        private void btn_sub_Click(object sender, EventArgs e)
+        {
+            Operator = "Subtraction";
+            tb_answer.Focus();
+            tb_answer.Enabled = true;
+            Start();
+        }
+
+        private void btn_mult_Click(object sender, EventArgs e)
+        {
+            Operator = "Multiplication";
+            tb_answer.Focus();
+            tb_answer.Enabled = true;
+            Start();
+        }
+
+        private void btn_div_Click(object sender, EventArgs e)
+        {
+            Operator = "Division";
+            tb_answer.Focus();
+            tb_answer.Enabled = true;
+            Start();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (timeForTimer < 60)
+            {
+                if (timeForTimer == 59)
+                {
+                    timeForTimer = 0;
+                    minuteTimeForTimer++;
+                    lab_timer.Text = $"{minuteTimeForTimer}:{timeForTimer}";
+                }
+                else
+                {
+                    timeForTimer++;
+                }
+                lab_timer.Text = $"{minuteTimeForTimer}:{timeForTimer}";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            tb_answer.Focus();
+            if (tb_answer.Text == "")
+            {
+                MessageBox.Show("ENTER ANSWER!");
+            }
+            else
+            {
+
+                if (progressBar1.Value < 100)
+                {
+
+                    progressBar1.Value += (progressBar1.Maximum / Max_Try);
+                    if (answer == Convert.ToInt32(tb_answer.Text))
+                    {
+                        Try++;
+                        RightAnswer++;
+                    }
+                    else
+                    {
+                        WrongAnswer++;
+                    }
+
+                    Consider();
+                    if (progressBar1.Value == 100)
+                    {
+                        progressBar1.Value = 0;
+                        Done();
+                    }
+                }
+            }
+        }
+    }
+}
